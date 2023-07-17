2a857a30d407e1886c4e93566f84e3eb5d6ad27d
villiseck <jvk@abes.fr>
2023-07-17 09:49:47 +0200

    Mise en place structure maven

---
+++ src/main/java/fr/abes/kafkatosudoc/KafkaToSudocApplication.java
@@ -0,0 +1,13 @@
+package fr.abes.kafkatosudoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaToSudocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaToSudocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/abes/kafkatosudoc/KafkaToSudocApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.abes.kafkatosudoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaToSudocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
