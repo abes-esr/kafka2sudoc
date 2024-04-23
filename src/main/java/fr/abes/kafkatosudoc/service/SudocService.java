8d37df695b78a122cb00bae26d058d7b53430e7b
pierre-maraval <pierre.maraval@gmail.com>
2024-04-23 12:05:26 +0200

    Fix : Mise à jour version accesCbs, prise en compte des erreurs de communication avec le cbs via système de retry

--- src/main/java/fr/abes/kafkatosudoc/service/SudocService.java
+++ src/main/java/fr/abes/kafkatosudoc/service/SudocService.java
@@ -6,10 +6,15 @@ import fr.abes.cbs.notices.Biblio;
 import fr.abes.cbs.notices.NoticeConcrete;
 import fr.abes.cbs.process.ProcessCBS;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.Level;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+
+import java.io.IOException;
 
 @Slf4j
 public class SudocService {
-	private ProcessCBS cbs;
+	private final ProcessCBS cbs;
 
 	public SudocService() {
 		this.cbs = new ProcessCBS();
@@ -22,17 +27,46 @@ public class SudocService {
 
 	/**
 	 * méthode d'authentification au Sudoc : on vérifie avant qu'une connexion n'est pas déjà établie
-	 *
+	 * @param serveurSudoc nom du serveur
+	 * @param portSudoc port de connexion
+	 * @param loginSudoc login d'authentification
+	 * @param passwordSudoc mot de passe
 	 * @throws CBSException erreur d'authentification
+	 * @throws IOException erreur de communication avec le CBS
 	 */
-	public void authenticate(String serveurSudoc, String portSudoc, String loginSudoc, String passwordSudoc) throws CBSException {
-		if (!isLogged())
-			this.cbs.authenticate(serveurSudoc, portSudoc, loginSudoc, passwordSudoc);
+	@Retryable(maxAttempts = 4, retryFor = IOException.class, noRetryFor = {CBSException.class}, backoff = @Backoff(delay = 1000, multiplier = 2) )
+	public void authenticate(String serveurSudoc, String portSudoc, String loginSudoc, String passwordSudoc) throws CBSException, IOException {
+		if (isNotLogged())
+			try {
+				this.cbs.authenticate(serveurSudoc, portSudoc, loginSudoc, passwordSudoc);
+			} catch (IOException ex) {
+				this.cbs.disconnect();
+				this.cbs.authenticate(serveurSudoc, portSudoc, loginSudoc, passwordSudoc);
+				throw ex;
+			}
 	}
 
-	public void authenticateBaseSignal(String serveurSudoc, String portSudoc, String loginSudoc, String passwordSudoc, String signalDB) throws CBSException {
-		if (!isLogged())
-			this.cbs.authenticateWithLogicalDb(serveurSudoc, portSudoc, loginSudoc, passwordSudoc, signalDB);
+	/**
+	 * Méthode d'authentification sur une base Signal
+	 * @param serveurSudoc nom du serveur
+	 * @param portSudoc port de connexion
+	 * @param loginSudoc login d'authentification
+	 * @param passwordSudoc mot de passe
+	 * @param signalDB numéro de la base signal
+	 * @throws CBSException erreur d'authentification
+	 * @throws IOException erreur de communication avec le CBS
+	 */
+	@Retryable(maxAttempts = 4, retryFor = IOException.class, noRetryFor = {CBSException.class}, backoff = @Backoff(delay = 1000, multiplier = 2) )
+	public void authenticateBaseSignal(String serveurSudoc, String portSudoc, String loginSudoc, String passwordSudoc, String signalDB) throws CBSException, IOException {
+		if (isNotLogged()) {
+			try {
+				this.cbs.authenticateWithLogicalDb(serveurSudoc, portSudoc, loginSudoc, passwordSudoc, signalDB);
+			} catch (IOException ex) {
+				this.cbs.disconnect();
+				this.cbs.authenticateWithLogicalDb(serveurSudoc, portSudoc, loginSudoc, passwordSudoc, signalDB);
+				throw ex;
+			}
+		}
 	}
 
 	/**
@@ -40,26 +74,26 @@ public class SudocService {
 	 *
 	 * @return true si authentifié
 	 */
-	private boolean isLogged() {
-		return this.cbs.getClientCBS().isLogged();
+	private boolean isNotLogged() {
+		return !this.cbs.getClientCBS().isLogged();
 	}
 
 	/**
 	 * Méthode récupérant une notice bouquet en fonction d'un nom de package et d'un provider
 	 *
-	 * @param provider
-	 * @param packageKbart
+	 * @param provider : nom du provider
+	 * @param packageKbart : nom du package kbart
 	 * @return le ppn de la notice récupérée
 	 * @throws CBSException erreur dans la recherche de la notice bouquet
 	 */
-	public String getNoticeBouquet(String provider, String packageKbart) throws CBSException {
+	public String getNoticeBouquet(String provider, String packageKbart) throws CBSException, IOException {
 		String query = "che bou " + provider + "_" + packageKbart;
 		log.debug("Recherche notice bouquet : " + query);
 		this.cbs.search(query);
 		if (this.cbs.getNbNotices() == 1) {
 			return cbs.getPpnEncours();
 		} else {
-			throw new CBSException("V/VERROR", "Provider : " + provider + " / package : " + packageKbart + " : Recherche de la notice bouquet échouée");
+			throw new CBSException(Level.WARN, "Provider : " + provider + " / package : " + packageKbart + " : Recherche de la notice bouquet échouée");
 		}
 	}
 
@@ -71,7 +105,7 @@ public class SudocService {
 	 * @throws CBSException  : Aucune notice ne correspondant au ppn
 	 * @throws ZoneException : erreur de construction de l'objet NoticeConcrete
 	 */
-	public NoticeConcrete getNoticeFromPpn(String ppn) throws CBSException, ZoneException {
+	public NoticeConcrete getNoticeFromPpn(String ppn) throws CBSException, ZoneException, IOException {
 		String query = "che ppn " + ppn;
 		log.debug("Recherche notice : " + query);
 		this.cbs.search(query);
@@ -80,7 +114,7 @@ public class SudocService {
 			notice = cbs.editerNoticeConcrete("1");
 			this.cbs.back();
 		} else {
-			throw new CBSException("V/VERROR", "Aucune notice ne correspond à la recherche sur le PPN " + ppn);
+			throw new CBSException(Level.WARN, "Aucune notice ne correspond à la recherche sur le PPN " + ppn);
 		}
 		return notice;
 	}
@@ -126,7 +160,7 @@ public class SudocService {
 	 * @throws ZoneException erreur de construction de la notice
 	 * @throws CBSException  erreur lors du passage en édition
 	 */
-	public NoticeConcrete passageEditionNotice(int noNotice) throws ZoneException, CBSException {
+	public NoticeConcrete passageEditionNotice(int noNotice) throws ZoneException, CBSException, IOException {
 		return cbs.editerNoticeConcrete(String.valueOf(noNotice));
 	}
 
@@ -137,7 +171,7 @@ public class SudocService {
 	 * @param noLigne numéro de notice dans le lot récupéré via une recherche antérieure
 	 * @throws CBSException erreur de modification de la notice
 	 */
-	public void modifierNotice(NoticeConcrete notice, int noLigne) throws CBSException {
+	public void modifierNotice(NoticeConcrete notice, int noLigne) throws CBSException, IOException {
 		this.cbs.modifierNoticeConcrete(String.valueOf(noLigne), notice);
 	}
 
@@ -147,7 +181,7 @@ public class SudocService {
 	 * @param notice notice à créer
 	 * @throws CBSException Erreur de création de notice
 	 */
-	public void creerNotice(NoticeConcrete notice) throws CBSException {
+	public void creerNotice(NoticeConcrete notice) throws CBSException, IOException {
 		this.cbs.enregistrerNew(notice.toString());
 	}
 
@@ -171,7 +205,7 @@ public class SudocService {
 	 * @return le nombre de notices liées
 	 * @throws CBSException erreur sur la commande CBS
 	 */
-	public int getNoticesLiees() throws CBSException {
+	public int getNoticesLiees() throws CBSException, IOException {
 		return this.cbs.rel();
 	}
 
@@ -180,7 +214,7 @@ public class SudocService {
 	 *
 	 * @throws CBSException erreur sur la commande CBS
 	 */
-	public void retourArriere() throws CBSException {
+	public void retourArriere() throws CBSException, IOException {
 		this.cbs.back();
 	}
 
@@ -190,7 +224,7 @@ public class SudocService {
 	 * @param pos la position de la notice dans le lot
 	 * @throws CBSException erreur sur la commande CBS
 	 */
-	public void voirNotice(int pos) throws CBSException {
+	public void voirNotice(int pos) throws CBSException, IOException {
 		log.debug("passage notice suivante");
 		this.cbs.view(String.valueOf(pos), false, "UNM");
 	}
